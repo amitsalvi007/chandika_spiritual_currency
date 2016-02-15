a88466fd67bb481c3eb7c66d0edaba6d0629e882
amitsalvi007 <amitsalvi007@gmail.com>
2016-02-15 22:17:00 +0530

    added click event

--- www/js/Home.js
+++ www/js/Home.js
@@ -1,1 +1,36 @@
+var myApp = angular.module('myApp', []);
 
+myApp.controller("controller", function($scope){
+	
+	// Fetch Device info from Device Plugin
+	$scope.alertDeviceInfo = function() {
+		
+        window.location = "joinUs.html"
+	};
+
+	// Fetch location info from GeoLocation Plugin
+	$scope.alertGeoLocation = function() {
+		var onSuccess = function(position) {
+			navigator.notification.alert('Latitude: '
+					+ position.coords.latitude + '\n' + 'Longitude: '
+					+ position.coords.longitude + '\n' + 'Altitude: '
+					+ position.coords.altitude + '\n' + 'Accuracy: '
+					+ position.coords.accuracy + '\n' + 'Altitude Accuracy: '
+					+ position.coords.altitudeAccuracy + '\n' + 'Heading: '
+					+ position.coords.heading + '\n' + 'Timestamp: '
+					+ position.timestamp + '\n');
+		};
+		navigator.geolocation.getCurrentPosition(onSuccess);
+
+	};
+
+	// Makes a beep sound
+	$scope.beepNotify = function() {
+		navigator.notification.beep(1);
+	};
+
+	// Vibrates the phone
+	$scope.vibrateNotify = function() {
+		navigator.notification.vibrate(1000);
+	};
+});
